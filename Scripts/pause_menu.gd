0569a50a70cba40a36a636378315db1521eb32dd
AD <aadi.indradevi@gmail.com>
2026-02-24 14:36:38 -0500

    Incorporated pause menu in game

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -3,11 +3,15 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	visible = false
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("pause"):
+		if (get_tree().paused==false ):
+			get_tree().paused= true
+			visible=true
 
 
 func _on_resume_pressed() -> void:

52491f93d624cf6fb7a6664a27c3918931a3292d
AD <aadi.indradevi@gmail.com>
2026-02-24 14:40:33 -0500

    Esc

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -7,12 +7,14 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed("pause"):
 		if (get_tree().paused==false ):
 			get_tree().paused= true
 			visible=true
-
+		elif (get_tree().paused ==true):
+			get_tree().paused= false
+			visible=false
 
 func _on_resume_pressed() -> void:
 	get_tree().paused = false

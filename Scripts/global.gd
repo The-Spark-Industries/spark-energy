ff2cb7eb8c02c52ccaf0669ed54d6cb1f5013854
AD <aadi.indradevi@gmail.com>
2026-03-01 17:55:54 -0500

    Basic inventory

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,6 +1,7 @@
 extends Node
 
-
+var inventory: Array = []
+var max_inventory_size= [100]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +10,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func add_item_to_inventory(item:int):
+	print(inventory)
+	for i in range(min(inventory.size(),max_inventory_size)):
+		if (inventory[i]==null):
+			inventory[i]=item
+			
+			return
+	if (inventory.size() < max_inventory_size):
+		inventory.append(item)
+		print(inventory)
+	else:
+		print("Your inventory is full!")
+				

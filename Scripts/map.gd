5bad9861c9cf8fee6fcb71f9e211a2a8dd02ee37
AD <aadi.indradevi@gmail.com>
2026-03-15 00:23:06 -0400

    Start

--- Scripts/map.gd
+++ Scripts/map.gd
@@ -4,6 +4,8 @@ extends Area2D
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	%notif.hide()
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.get_meta("tag", "") != "player":
@@ -11,6 +13,14 @@ func _on_body_entered(body: Node2D) -> void:
 		return
 
 	
-
 	Global.add_item_to_inventory("Map")
-	queue_free()
+	%notif.show()
+	%notif.visible=true
+	get_tree().paused= true
+	%CollisionShape2D.queue_free()
+	%Sprite2D.queue_free()
+
+
+func _on_ok_pressed() -> void:
+	get_tree().paused= false
+	%notif.hide()

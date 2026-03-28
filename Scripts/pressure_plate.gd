d3bb8301c88763a8512c7b44101344de0923745e
AD <aadi.indradevi@gmail.com>
2026-03-27 21:54:38 -0400

    Pressure plate, valve

--- Scripts/pressure_plate.gd
+++ Scripts/pressure_plate.gd
@@ -3,10 +3,10 @@ extends Area2D
 
 var readyToPress: bool= false
 #Checks if the player is near the valve
-@onready var valvestatus: int = 0
+@onready var pressurePlateStatus: int = 0
 #Determines the valve state
 
-@onready var change= $valveSprites
+@onready var change= $pressurePlateSprites
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,21 +14,20 @@ func _ready() -> void:
 
 # # Changes the sprite to whatever the valve status is.
 func _process(delta: float) -> void:
-	change.frame=valvestatus
+	change.frame=pressurePlateStatus
 	
 		
 
 
-func _input(event: InputEvent) -> void:
-	if (Input.is_action_just_pressed("interact")) and (readyToPress==true) and (valvestatus<4):
-		valvestatus+=1
-		print(valvestatus)
+
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body is CharacterBody2D):
 		readyToPress= true
+		pressurePlateStatus=1
 
 func _on_body_exited(body: Node2D) -> void:
 	if (body is CharacterBody2D):
 		readyToPress= false
+		pressurePlateStatus=0

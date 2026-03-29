21eb5638744db3bbcb5d3aa90922c5559a1eb9dd
AD <70617945+AD-The-Programmer@users.noreply.github.com>
2026-03-29 17:05:11 -0400

    Levers buttons gameplay (#93)
    * Added buttons/levers
    
    * Pressure plate, valve

---
+++ Scripts/pressure_plate.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+
+var readyToPress: bool= false
+#Checks if the player is near the valve
+@onready var pressurePlateStatus: int = 0
+#Determines the valve state
+
+@onready var change= $pressurePlateSprites
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# # Changes the sprite to whatever the valve status is.
+func _process(delta: float) -> void:
+	change.frame=pressurePlateStatus
+	
+		
+
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if (body is CharacterBody2D):
+		readyToPress= true
+		pressurePlateStatus=1
+
+func _on_body_exited(body: Node2D) -> void:
+	if (body is CharacterBody2D):
+		readyToPress= false
+		pressurePlateStatus=0

aaf341d63a97e49a492a9f40249ed73d1641c145
c-fido <gfedolfi@wesleyan.edu>
2026-04-06 16:03:47 -0400

    Tileset Push

--- Scripts/lilypad.gd
+++ Scripts/lilypad.gd
@@ -1,25 +1,47 @@
 extends StaticBody2D
 
 
+@export var sink_distance: float = 128
+
+var original_position: Vector2
+var activated: bool = false
+var sink_cycle_running: bool = false
+
+@onready var trigger_area: Area2D = $Area2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	original_position = position
+	trigger_area.monitoring = true
+	trigger_area.monitorable = true
+	if not trigger_area.body_entered.is_connected(_on_area_2d_body_entered):
+		trigger_area.body_entered.connect(_on_area_2d_body_entered)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(_delta: float) -> void:
+	if activated or sink_cycle_running:
+		return
 
-
-
-	
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	if (body is CharacterBody2D):
-		queue_free()
+	for body in trigger_area.get_overlapping_bodies():
+		if body is CharacterBody2D:
+			_start_sink_cycle()
+			break
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if (body is CharacterBody2D):
-		await get_tree().create_timer(2.0).timeout
-		queue_free()
+	if body is CharacterBody2D:
+		_start_sink_cycle()
+
+
+func _start_sink_cycle() -> void:
+	if activated or sink_cycle_running:
+		return
+
+	sink_cycle_running = true
+	activated = true
+	await get_tree().create_timer(0.2).timeout
+	position = original_position + Vector2(0.0, sink_distance)
+	await get_tree().create_timer(3).timeout
+	position = original_position
+	activated = false
+	sink_cycle_running = false

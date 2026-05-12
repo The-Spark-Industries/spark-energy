0d2888dbe082670bd3dab5288186f91deb263118
c-fido <gfedolfi@wesleyan.edu>
2026-05-11 22:51:58 -0400

    Fixes

--- Scripts/PipeMinigame.gd
+++ Scripts/PipeMinigame.gd
@@ -123,8 +123,8 @@ func _ready() -> void:
 		_root_panel.add_theme_stylebox_override("panel", StyleBoxEmpty.new())
 		$CenterContainer/PanelContainer/VBoxContainer/Title.visible = false
 		$CenterContainer/PanelContainer/VBoxContainer/Info.visible = false
-		$CenterContainer/PanelContainer/VBoxContainer/Footer.visible = true
-		$CenterContainer/PanelContainer/VBoxContainer/Footer/Status.visible = true
+		$CenterContainer/PanelContainer/VBoxContainer/Footer.visible = false
+		$CenterContainer/PanelContainer/VBoxContainer/Footer/Status.visible = false
 		$CenterContainer/PanelContainer/VBoxContainer/Footer/Status.modulate = Color(1, 1, 1, 0)
 		_ensure_embedded_rect_size()
 		if Engine.is_editor_hint():
@@ -1056,15 +1056,15 @@ func _piece_texture(piece: Dictionary, is_powered: bool = false) -> Texture2D:
 			return sink_texture
 		"straight":
 			if is_powered:
-				return _resolve_variant_texture(powered_straight_textures, dirt_level, powered_straight_texture if powered_straight_texture else straight_texture)
+				return _resolve_variant_texture(powered_straight_textures, dirt_level, straight_texture)
 			return _resolve_variant_texture(straight_textures, dirt_level, straight_texture)
 		"corner":
 			if is_powered:
-				return _resolve_variant_texture(powered_corner_textures, dirt_level, powered_corner_texture if powered_corner_texture else corner_texture)
+				return _resolve_variant_texture(powered_corner_textures, dirt_level, corner_texture)
 			return _resolve_variant_texture(corner_textures, dirt_level, corner_texture)
 		"tee":
 			if is_powered:
-				return _resolve_variant_texture(powered_tee_textures, dirt_level, powered_tee_texture if powered_tee_texture else tee_texture)
+				return _resolve_variant_texture(powered_tee_textures, dirt_level, tee_texture)
 			return _resolve_variant_texture(tee_textures, dirt_level, tee_texture)
 		"block":
 			if is_powered and powered_block_texture:
